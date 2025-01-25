95681c5b4f71accc04340e1dcbaee0ea42e10e62
Iman Irajdoost <iman.milo@yahoo.com>
2025-01-25 12:58:23 +0100

    Ca avance

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,6 +13,14 @@ public class MovementController : MonoBehaviour
     [SerializeField] private float _speed = 10;
 
     [SerializeField] private float _jumpForce = 10;
+
+    [SerializeField] private float _stopJumpingTime = 0.5f;
+
+    [SerializeField] private float _gravityMultiplier = 10;
+
+    private bool _canJump = true;
+
+    private Coroutine _jumpCoroutine;
 
     private void Awake()
     {
@@ -34,22 +43,62 @@ public class MovementController : MonoBehaviour
         Vector2 moveValue = moveAction.ReadValue<Vector2>();
 
         // Go to left or right
-        rb.linearVelocity = new Vector3(_speed * moveValue.x * Time.deltaTime, rb.linearVelocity.y, rb.linearVelocity.z);
+        rb.linearVelocity = new Vector3(_speed * moveValue.x * Time.fixedDeltaTime, rb.linearVelocity.y, rb.linearVelocity.z);
 
-        if (jumpAction.IsPressed() && IsGrounded())
+        bool isGrounded = IsGrounded();
+
+        bool isWallDetected = IsWallDetected();
+
+        if (jumpAction.IsPressed() && CanJump() && isGrounded)
         {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, _jumpForce * Time.deltaTime, rb.linearVelocity.z);
+            rb.AddForce(new Vector3(0, _jumpForce * Time.fixedDeltaTime, 0), ForceMode.Force);
+            //rb.linearVelocity =  new Vector3(rb.linearVelocity.x, _jumpForce * Time.fixedDeltaTime, rb.linearVelocity.z);
+
+            if (_jumpCoroutine != null)
+                StopCoroutine(_jumpCoroutine);
+            _jumpCoroutine = StartCoroutine(StopJumpingFor(_stopJumpingTime));
         }
+
+        if(!isGrounded)
+        {
+            // simulate gravity
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y - (_gravityMultiplier * Time.fixedDeltaTime), rb.linearVelocity.z);
+        }
+    }
+
+    private IEnumerator StopJumpingFor(float delay)
+    {
+        _canJump = false;
+        yield return new WaitForSeconds(delay);
+        _canJump = true;
+    }
+
+    private bool CanJump()
+    {
+        return _canJump;
+    }
+
+    private bool IsWallDetected()
+    {
+        var cols = Physics.OverlapBox(new Vector3(col.bounds.center.x + 0.25f, 0, 0), col.bounds.extents);
+        foreach (var c in cols)
+        {
+            if (c.gameObject != gameObject)
+                return true;
+        }
+        return false;
     }
 
     private bool IsGrounded()
     {
-        Vector3 point1 = new Vector3(col.transform.position.x, col.transform.position.y / 4);
-        Vector3 point2 = new Vector3(col.transform.position.x, col.transform.position.y / 4);
-        var cols = Physics.OverlapCapsule(point1, point2, col.radius);
+        var cols = Physics.OverlapSphere(new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius / 2);
 
-        if (cols == null || cols.Length == 0)
-            return false;
-        return true;
+        foreach (var c in cols)
+        {
+            if (c.gameObject != gameObject)
+                return true;
+        }
+
+        return false;
     }
 }
